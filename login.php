dbeb804aa6eddd95878b3eba45d727219589e0dd
dolya-julia <113310097+dolya-julia@users.noreply.github.com>
2023-04-27 22:45:17 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Файл login.php для не авторизованного пользователя выводит форму логина.
  * При отправке формы проверяет логин/пароль и создает сессию,
@@ -7,14 +6,10 @@
  * После авторизации пользователь перенаправляется на главную страницу
  * для изменения ранее введенных данных.
  **/
-
 // Отправляем браузеру правильную кодировку,
 // файл login.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-
-// Начинаем сессию.
-session_start();
-
+include_once('Addition.php');
 // В суперглобальном массиве $_SESSION хранятся переменные сессии.
 // Будем сохранять туда логин после успешной авторизации.
 if (!empty($_SESSION['login'])) {
@@ -24,31 +19,31 @@ if (!empty($_SESSION['login'])) {
   // Делаем перенаправление на форму.
   header('Location: ./');
 }
-
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
-
-<form action="" method="post">
+<form action="login.php" method="post">
   <input name="login" />
   <input name="pass" />
   <input type="submit" value="Войти" />
 </form>
-
 <?php
 }
 // Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
 else {
-
-  // TODO: Проверть есть ли такой логин и пароль в базе данных.
-  // Выдать сообщение об ошибках.
-
-  // Если все ок, то авторизуем пользователя.
-  $_SESSION['login'] = $_POST['login'];
-  // Записываем ID пользователя.
-  $_SESSION['uid'] = 123;
-
-  // Делаем перенаправление.
-  header('Location: ./');
+  $stmt = $db->prepare("SELECT * FROM main WHERE `username` = :username");
+  $stmt->execute(['username' => $_POST['login']]);
+  $user = $stmt->fetch(PDO::FETCH_ASSOC);
+  if (!$stmt->rowCount()) {
+    printf('Пользователь с такими данными не зарегистрирован');
+  }
+  else if(!password_verify($_POST['pass'], $user['password'])) {
+    printf('Пароль неверен');
+  }
+  else {
+    $_SESSION['login'] = $_POST['login'];
+    header('Location: ./');
+  }
 }
+
